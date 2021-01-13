d0a3b2c4097658865996a2ee4d2602e13b701563
Gavin <grncheetah10@gmail.com>
2021-01-13 16:55:12 -0600

    Added event command base, tables for injects and members

--- DBManager.py
+++ DBManager.py
@@ -1,6 +1,49 @@
+import os
+import re
 import sqlite3
 
-import tableSetup
+import tableSetup as ts
+
+def is_safe_path(basedir, path, follow_symlinks=True):
+    """Makes sure paths are gucci"""
+    # Taken from
+    # https://security.openstack.org/guidelines/dg_using-file-paths.html
+    # resolves symbolic links
+    if follow_symlinks:
+        return os.path.realpath(path).startswith(basedir)
+
+    return os.path.abspath(path).startswith(basedir)
+
+
+def create_event(event_name):
+    """Creates a new database file with all relevant tables.
+    
+    Returns whether or not the event was created
+    """
+    event_name.replace(" ", "_")
+    db_path = os.path.join("data", str(event_name) + ".db")
+    
+    # Check if the path is fine, otherwise fail to create the event
+    if not is_safe_path(os.getcwd(), db_path):
+        return False
+
+    # Check to see if this event exists
+    if os.path.exists(db_path):
+        # TODO Load the event
+        
+    else:
+        # Create the db
+        conn = sqlite3.connect(db_path)
+        curs = conn.cursor()
+        # Create the tables
+        curs.execute(ts.tbl_members)
+        curs.execute(ts.tbl_injects)
+
+    conn.commit()
+    conn.close()
+
+    return True
+
 
 
 def open_database():
